fc5d550746a8453384d95eae3881d71e61d70ce8
Bhawandeep Kaur <734810@student.tor.loc>
2019-09-27 14:22:41 -0400

    updated

--- activity1.js
+++ activity1.js
@@ -1,5 +1,5 @@
 var http = require('http');
- 
+const sqlite3 = require('sqlite3').verbose();
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   // res.write("The date and time are currently: " + dt.myDateTime());
